444edb1e791523ef8c716f369289c9c161db701d
Yoné Rocha <yone@yonerocha.net>
2023-08-02 12:37:55 +0100

    [ADD] sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config'
-import yaml from '@rollup/plugin-yaml'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import yaml from '@rollup/plugin-yaml';
+import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +10,14 @@ export default defineConfig({
   vite: {
     plugins: [yaml()]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false // a True carga los archivos base de tailwind sin tener que crear un base.css e importarlo en el layout
-    })
-  ]
-})
+  integrations: [tailwind({
+    applyBaseStyles: false // a True carga los archivos base de tailwind sin tener que crear un base.css e importarlo en el layout
+  }), sitemap({
+    filter: (page) => {
+      // Omitir manifes.json y splash del sitemap
+      //console.log('hola' + page)
+      return page !== `${import.meta.env.VITE_URL}/manifest.json/` &&
+      page !== `${import.meta.env.VITE_URL}/splash/`
+    }
+  })]
+});
