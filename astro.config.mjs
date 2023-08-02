8d6e7c0e697abc1658e597bab29551344030ec8c
Yoné Rocha <yone@yonerocha.net>
2023-08-02 13:53:17 +0100

    [UDPATE] Bug manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import yaml from '@rollup/plugin-yaml';
-import tailwind from '@astrojs/tailwind';
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import yaml from '@rollup/plugin-yaml'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,14 +10,16 @@ export default defineConfig({
   vite: {
     plugins: [yaml()]
   },
-  integrations: [tailwind({
-    applyBaseStyles: false // a True carga los archivos base de tailwind sin tener que crear un base.css e importarlo en el layout
-  }), sitemap({
-    filter: (page) => {
-      // Omitir manifes.json y splash del sitemap
-      //console.log('hola' + page)
-      return page !== `${import.meta.env.VITE_URL}/manifest.json/` &&
-      page !== `${import.meta.env.VITE_URL}/splash/`
-    }
-  })]
-});
+  integrations: [
+    tailwind({
+      applyBaseStyles: false // a True carga los archivos base de tailwind sin tener que crear un base.css e importarlo en el layout
+    }),
+    sitemap({
+      filter: (page) => {
+        // Omitir manifest.json y splash del sitemap
+        //console.log('hola' + page)
+        return page !== `${import.meta.env.VITE_URL}/splash/`
+      }
+    })
+  ]
+})
